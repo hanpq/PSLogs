88cd964f017989cfebed447c6046677192c5df55
Hannes Palmquist <hannes.palmquist@outlook.com>
2023-08-30 13:53:41 +0200

    TS github actions

---
+++ .build/Unload.Module.build.ps1
@@ -0,0 +1,34 @@
+﻿param (
+    # Base directory of all output (default to 'output')
+    [Parameter()]
+    [string]
+    $OutputDirectory = (property OutputDirectory (Join-Path -Path $BuildRoot -ChildPath 'output')),
+
+    [Parameter()]
+    [System.String]
+    $BuiltModuleSubdirectory = (property BuiltModuleSubdirectory ''),
+
+    [Parameter()]
+    [System.String]
+    $ProjectName = (property ProjectName ''),
+
+    [Parameter()]
+    [System.String]
+    $BuildModuleOutput = (property BuildModuleOutput (Join-Path $OutputDirectory $BuiltModuleSubdirectory)),
+
+    [Parameter()]
+    [string]
+    $PFX_BASE64 = (property PFX_BASE64),
+
+    [Parameter()]
+    [string]
+    $PFX_PASS = (property PFX_PASS 'test')
+)
+
+# Synopsis: Deleting the content of the Build Output folder, except ./modules
+Task Unload_Module {
+    . Set-SamplerTaskVariable
+
+    Remove-Module $ProjectName
+
+}
