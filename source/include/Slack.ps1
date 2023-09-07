7382db05e9b2b6a6f1b37d2cd315b381033a4898
Hannes Palmquist <hannes.palmquist@outlook.com>
2023-09-07 14:00:51 +0200

    Added default icon values for new logging levels

--- source/include/Slack.ps1
+++ source/include/Slack.ps1
@@ -1,20 +1,26 @@
 @{
-    Name = 'Slack'
+    Name          = 'Slack'
     Configuration = @{
         WebHook = @{Required = $true; Type = [string]; Default = $null }
         BotName = @{Required = $false; Type = [string]; Default = $null }
         Channel = @{Required = $false; Type = [string]; Default = $null }
         Icons   = @{Required = $false; Type = [hashtable]; Default = @{
-                'ERROR'   = ':fire:'
-                'WARNING' = ':warning:'
-                'INFO'    = ':exclamation'
-                'DEBUG'   = ':eyes:'
+                'ERROR'     = ':fire:'
+                'WARNING'   = ':warning:'
+                'INFO'      = ':exclamation:'
+                'DEBUG'     = ':eyes:'
+                'NOTICE'    = ':speech_balloon:'
+                'VERBOSE'   = ':mag:'
+                'SUCCESS'   = ':white_check_mark:'
+                'CRITICAL'  = ':bangbang:'
+                'ALERT'     = ':x:'
+                'EMERGENCY' = ':red_circle:'
             }
         }
-        Level   = @{Required = $false; Type = [string]; Default = $Logging.Level}
-        Format  = @{Required = $false; Type = [string]; Default = $Logging.Format}
+        Level   = @{Required = $false; Type = [string]; Default = $Logging.Level }
+        Format  = @{Required = $false; Type = [string]; Default = $Logging.Format }
     }
-    Logger = {
+    Logger        = {
         param(
             [hashtable] $Log,
             [hashtable] $Configuration
@@ -24,9 +30,15 @@
             text = Format-Pattern -Pattern $Configuration.Format -Source $Log
         }
 
-        if ($Configuration.BotName) { $Text['username'] = $Configuration.BotName }
+        if ($Configuration.BotName)
+        {
+            $Text['username'] = $Configuration.BotName
+        }
 
-        if ($Configuration.Channel) { $Text['channel'] = $Configuration.Channel }
+        if ($Configuration.Channel)
+        {
+            $Text['channel'] = $Configuration.Channel
+        }
 
         $Text['icon_emoji'] = $Configuration.Icons[$Log.LevelNo]
 
